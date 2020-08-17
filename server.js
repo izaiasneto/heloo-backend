7e1a34f8beffd3d603063329ee9c9105ca0e29a2
Izaias neto <izaias_neto_@live.com>
2020-08-17 17:01:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const requireDir = require("require-dir")
 
 MONGO_URL = 'mongodb://localhost:27017/grcupomapi'
+
 //iniciando o APP
 const app = express()
 app.use(express.json()) //permitir que eu envie dados para aplicação em formato de json.
@@ -21,4 +22,6 @@ requireDir('./src/models')
 // Rotas
 app.use('/', require('./src/routes'))
 
-app.listen(process.env.PORT || 3001)
+app.listen(process.env.PORT || 3001, function(){
+    console.log('listening on', http.address().port);
+  });
