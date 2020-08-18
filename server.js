c2ea2e4d6e400045fd2deaa5d94149c28049842c
Izaias neto <izaias_neto_@live.com>
2020-08-18 13:48:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ var MONGO_URL = process.env.MONGO_URL
 var app = express();
 app.use(express.json())
 
+// CORS
+app.all("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-type, Accept, X-Access-Token, X-Key"
+    );
+    if ("OPTIONS" == req.method) res.status(200).end();
+    else next();
+});
+
 mongoose.set('useFindAndModify', false);
 
 // Iniciando o DB
