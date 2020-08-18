7c31d46e9ee08b78717f1d9b3ff7e69fc0935a71
Izaias neto <izaias_neto_@live.com>
2020-08-18 00:59:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var MONGO_URL = process.env.MONGO_URL  || 'mongodb://localhost:27017/grcupomapi'
 //iniciando o APP
 const app = express()
 app.use(express.json()) //permitir que eu envie dados para aplicação em formato de json.
-app.options('*', cors()) // include before other routes 
 app.use(cors())
 
 mongoose.set('useFindAndModify', false);
@@ -26,5 +25,12 @@ requireDir('./src/models')
 
 // Rotas
 app.use('/', require('./src/routes'))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.listen(process.env.PORT || 3001)
