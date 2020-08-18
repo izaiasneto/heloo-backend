1c6269c9c229028294bd4c7467aa917965c14111
Izaias neto <izaias_neto_@live.com>
2020-08-18 15:58:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
