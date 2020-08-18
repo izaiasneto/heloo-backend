ebdede9d29d4f8f62145af82be258c97bab2076a
Izaias neto <izaias_neto_@live.com>
2020-08-18 02:11:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const requireDir = require("require-dir")
 
 var MONGO_URL = process.env.MONGO_URL  
 
+var app = express();
 
-let app = express();
 app.use(cors());
-app.options('*', cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 mongoose.set('useFindAndModify', false);
 
